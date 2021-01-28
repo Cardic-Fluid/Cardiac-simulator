58315bb86f512573a8cab550bd4c55af3e7d83d0
chloegoemans <chloe.goemans@hotmail.com>
2021-01-28 15:54:02 +0100

    differential equations

--- Cardiac simulator.py
+++ Cardiac simulator.py
@@ -15,17 +15,7 @@ import math
 import numpy as np
 from matplotlib import pyplot as plt
 from scipy.integrate import odeint
-
-# Definition of the parameters with physiological mean values
-Prl = 5
-Pla = 8
-Pra = 3
-Rmv = 0.005
-Rav = 0.1
-Rao = 0.0398
-Ls = 1
-Cs = 1.41
-Rs = 1
+from sympy import *
 
 
 HR = 60
@@ -42,15 +32,48 @@ def elastance(tn):
     En = 1.55 * (((tn / 0.7) ** 1.9) / (1 + ((tn / 0.7) ** 1.9))) * (1 / (1 + ((tn / 1.17) ** 21.9)))
     E = (Emax-Emin)*En+Emin
     return E
-
+"""
 plt.plot(t, elastance(tn))
 plt.xlabel('time [s]')
 plt.ylabel('Elastance [mmHg/mL]')
 plt.show()
 """
-coefm = 
+
+
 
 # Differential equations
-def model (Vlv,t)
-    dVlvdt =
-"""
+def model (z,t,Vlv,Pp,Pao,tn):
+    # Definition of the parameters
+    Prl = 5
+    Pla = 8
+    Pra = 3
+    Rmv = 0.005
+    Rav = 0.1
+    Rao = 0.0398
+    Ls = 1
+    Cs = 1.41
+    Rs = 1
+    V0 = 12
+
+    # Definition of variables
+    Vlv = z[0]
+    Ps = z[1]
+    Pao = z[2]
+    E= elastance(tn)
+    Plv = E*(Vlv-V0)
+
+    Eprime = np.diff(E)
+
+    coefav = math.tanh(1000(Plv - Pao) + 1) / 2
+    coefmv = math.tanh(1000(Pla - Plv) + 1) / 2
+
+    #Differential equations
+    dVlvdt= coefmv* (Pla -E*(Vlv-V0))/Rmv - coefav * (E*(Vlv-V0-Pao))/Rav
+    dPsdt = coefav * (E * (Vlv - V0) - Pao) / (Rav * Cs) - (Ps - Pra) / (Rs * Cs)
+    dPaodt = (1/(1+(coefav*Rao/Rav)))*((Eprime*(Vlv-V0)+E*dVlvdt) * (Rao/Rav) * coefav + dPsdt + (Rao / Ls) * (Ps - Pao))
+    return [dVlvdt, dPsdt, dPaodt]
+
+# Initial conditions
+Vlv0 = 70
+Pp0 =
+Pao0 =
