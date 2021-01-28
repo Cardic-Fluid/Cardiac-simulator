777f772147f6b3fdc94c59dd0a22d9fda388d87e
chloegoemans <chloe.goemans@hotmail.com>
2021-01-28 16:44:40 +0100

    differential equations

--- Cardiac simulator.py
+++ Cardiac simulator.py
@@ -3,7 +3,6 @@ Cardiac simulator
 MEDI - H506 : Fluid mechanics of the cardiovascular and pulmonary systems. From physiology to applications.
 By Laura Estaire, Chloé Goemans, Laurine Martin & Susanna Rodrigues
 """
-
 """
 PARAMETERS USED IN THE SIMULATION
 
@@ -24,14 +23,16 @@ Tmax = 0.2+0.15*tc
 t = np.arange(0, 1, 0.01)
 tn = t / Tmax
 
-# Definition of the elastance function
 
+# Definition of the elastance function
 def elastance(tn):
     Emax = 2
     Emin = 0.06
     En = 1.55 * (((tn / 0.7) ** 1.9) / (1 + ((tn / 0.7) ** 1.9))) * (1 / (1 + ((tn / 1.17) ** 21.9)))
     E = (Emax-Emin)*En+Emin
     return E
+
+
 """
 plt.plot(t, elastance(tn))
 plt.xlabel('time [s]')
@@ -40,11 +41,10 @@ plt.show()
 """
 
 
-
 # Differential equations
-def model (x,t,Tmax):
+def model(x, t, tn):
     # Definition of the parameters
-    Prl = 5
+    # Prl = 5
     Pla = 8
     Pra = 3
     Rmv = 0.005
@@ -55,8 +55,6 @@ def model (x,t,Tmax):
     Rs = 1
     V0 = 12
 
-    tn = t / Tmax
-
     # Definition of variables
     Vlv = x[0]
     Ps = x[1]
@@ -64,32 +62,25 @@ def model (x,t,Tmax):
     E= elastance(tn)
     Plv = E*(Vlv-V0)
 
-    E1 = np.transpose(E)
-    return E1
-print(model ([1,1,1],t,Tmax))
-plt.plot(tn,E)
-plt.xlabel('time [s]')
-plt.ylabel('Elastance [mmHg/mL]')
-plt.show()
-"""
     Eprime = np.diff(E)
 
     coefav = math.tanh(1000(Plv - Pao) + 1) / 2
     coefmv = math.tanh(1000(Pla - Plv) + 1) / 2
 
-    #Differential equations
-    dVlvdt= coefmv* (Pla -E*(Vlv-V0))/Rmv - coefav * (E*(Vlv-V0-Pao))/Rav
+    # Differential equations
+    dVlvdt= coefmv * (Pla - E*(Vlv-V0))/Rmv - coefav * (E*(Vlv-V0-Pao))/Rav
     dPsdt = coefav * (E * (Vlv - V0) - Pao) / (Rav * Cs) - (Ps - Pra) / (Rs * Cs)
-    dPaodt = (1/(1+(coefav*Rao/Rav)))*((Eprime*(Vlv-V0)+E*dVlvdt) * (Rao/Rav) * coefav + dPsdt + (Rao / Ls) * (Ps - Pao))
+    dPaodt = (1/(1+(coefav*Rao/Rav)))*((Eprime*(Vlv-V0)+E*dVlvdt) * (Rao/Rav) * coefav+dPsdt+(Rao / Ls)*(Ps - Pao))
+
     return [dVlvdt, dPsdt, dPaodt]
+
 
 # Initial conditions
 Vlv0 = 120
 Pp0 = 20
 Pao0 = 80
 
-y = odeint(model,[Vlv0,Pp0,Pao0],t,args=(Tmax,))
-Vlv = y[:,0]
-Pp = y[:,1]
-Pao=y[:,2]
-"""
+y = odeint(model, [Vlv0, Pp0, Pao0], t, args=(tn,))
+Vlv = y[:, 0]
+Pp = y[:, 1]
+Pao = y[:, 2]
