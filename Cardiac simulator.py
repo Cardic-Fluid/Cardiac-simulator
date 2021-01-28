702c5974d61f4ac8b2aafc0cdff0f7426ed40871
chloegoemans <chloe.goemans@hotmail.com>
2021-01-28 10:33:12 +0100

    defining parameters

--- Cardiac simulator.py
+++ Cardiac simulator.py
@@ -6,48 +6,25 @@ By Laura Estaire, Chloé Goemans, Laurine Martin & Susanna Rodrigues
 
 """
 PARAMETERS USED IN THE SIMULATION
-Q = mean flow rate (steady state)
-Pf = Right atrial filling pressure
-Ca = 2 ml/mmHg = equivalent capacitance of the arteries 
-Cv = 100 ml/mmHg = total capacitance of all the veins in the systemic circulation
-Ra = 1 mmHg/ml/sec = peripheral resistance
-Rv = 0.06 mmHg/ml/sec = resistance to veinous flow
-V0 = 3000 ml = zero-pressure filling volume (Volume that fills the undistended system)
-V_t = 4000 ml = total blood volume in the peripherical circulation (85 % of total blood volume)
-Pms = Mean systemic filling pressure 
+
+
 
 
 """
 import math
 
 # Definition of the parameters with physiological mean values
-Ca = 2
-Cv = 100
-Ra = 1
-Rv = 0.06
-V0 = 3000
-Va0 = 500
-Vv0 = V0 - Va0
-Vt = 4000
+Prl = 5
+Pla = 8
+Pra = 3
+Rmv = 0.005
+Rav = 0.1
+Rao = 0.0398
+Ls = 1
+Cs = 1.41
+Rs = 1
 
-Pf = 8
-Pa = 120
-Cs = 0.4
-Cd = 14
-Vd = 15
 
-"""
-Pms = (Vt-V0)/(Ca+Cv)
-Q = (Pms -Pf)/(Rv+Ra(Ca/(Ca+Cv)))
-"""
-# We don't know Pf => How to determine it?
+# Differential equations
 
-if Pl > Pa:
-    Qin = 0
-else:
-    Qin = (Pa-Pl)/Ra
 
-if Pl<Paorta:
-    Qout = 0
-else:
-    Qout = (Pl-Paorta)/RAV
